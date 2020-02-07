7e0ce3611efa2e167a98c678ce52c8b07c439e3e
Lem <bytehala@users.noreply.github.com>
2020-02-07 17:29:02 +0800

    put images in

--- test1.py
+++ test1.py
@@ -1,3 +1,18 @@
-import os
+import os.path
 
-print(os.scandir())
+
+images = os.scandir(os.getcwd() + "")
+#print(images)
+sortedimages = sorted(images, key=lambda x: x.stat().st_mtime, reverse=False)
+newlist = []
+
+names = ['one.png', 'two.png', 'three.png']
+index = 0
+for i in sortedimages:
+	if(i.name.endswith(".png")):
+		#print(i.stat().st_ctime)
+		print(i.stat())
+		print(i.name)
+		print(names[index])
+#		os.rename("" + i.name, "" + names[index])
+		index = index + 1
